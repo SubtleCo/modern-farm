41e6f9f6a537340f57915913afffacecb8e8fced
Alex Martin <alex@subtleco.co>
2021-01-14 12:02:24 -0600

    Added harvester.js that multiplies each seed object into multiple plants based on output, save corn

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,13 @@ console.log("Welcome to the main module")
 import { createPlan } from './plan.js';
 import { usePlants } from './field.js';
 import { plantSeeds } from './tractor.js';
+import { harvestPlants } from './harvester.js';
 
 const yearlyPlan = createPlan();
-console.log(yearlyPlan)
+plantSeeds(yearlyPlan);
 
-plantSeeds(yearlyPlan)
 const seedPlot = usePlants();
 console.log(seedPlot)
+const grownPlants = harvestPlants(seedPlot);
+
+console.log(grownPlants);
